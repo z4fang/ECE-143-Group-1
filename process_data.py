ce2fa0a31cc90c5a21a717db9009099ea65dde50
jeangorby <calicdanjeangorby@gmail.com>
2023-03-04 23:13:13 -0800

    added all worldcup matches and winners to clean data

--- process_data.py
+++ process_data.py
@@ -23,6 +23,9 @@ gdp_per_capita = pd.read_csv('Data/gdp_per_capita.csv')
 gdp_ppp_per_capita = pd.read_csv('Data/gdp_ppp_per_capita.csv')
 gdp_ppp = pd.read_csv('Data/gdp_ppp.csv')
 
+world_cup_matches = pd.read_csv('Data/world_cup_matches.csv')
+world_cups = pd.read_csv('Data/world_cups.csv')
+
 # Replace all spaces with underscores
 gdp = replace_spaces(gdp)
 gdp_growth = replace_spaces(gdp_growth)
@@ -31,8 +34,8 @@ gdp_per_capita = replace_spaces(gdp_per_capita)
 gdp_ppp_per_capita = replace_spaces(gdp_ppp_per_capita)
 gdp_ppp = replace_spaces(gdp_ppp)
 
-world_cup_matches = pd.read_csv('Data/world_cup_matches.csv')
 world_cup_matches = replace_spaces(world_cup_matches)
+world_cups = replace_spaces(world_cups)
 
 # Removing countries that weren't in the world cup from all the GDP data
 index = gdp.country_name.isin(world_cup_matches.home_team) | gdp.country_name.isin(world_cup_matches.away_team)
@@ -44,9 +47,15 @@ gdp_ppp_per_capita = gdp_ppp_per_capita[index]
 gdp_ppp = gdp_ppp[index]
 
 # Write the cleaned data back into csv files in Clean_Data folder
-gdp.to_csv('Clean_data/gdp.csv')
-gdp_growth.to_csv('Clean_data/gdp_growth.csv')
-gdp_per_capita_growth.to_csv('Clean_data/gdp_per_capita_growth.csv')
-gdp_per_capita.to_csv('Clean_data/gdp_per_capita.csv')
-gdp_ppp_per_capita.to_csv('Clean_data/gdp_ppp_per_capita.csv')
-gdp_ppp.to_csv('Clean_data/gdp_ppp.csv')
+gdp.to_csv('Clean_Data/gdp.csv')
+gdp_growth.to_csv('Clean_Data/gdp_growth.csv')
+gdp_per_capita_growth.to_csv('Clean_Data/gdp_per_capita_growth.csv')
+gdp_per_capita.to_csv('Clean_Data/gdp_per_capita.csv')
+gdp_ppp_per_capita.to_csv('Clean_Data/gdp_ppp_per_capita.csv')
+gdp_ppp.to_csv('Clean_Data/gdp_ppp.csv')
+
+# Write world_cup_matches into csv as all_world_cup_matches
+world_cup_matches.to_csv('Clean_Data/all_world_cup_matches.csv')
+
+# Write world_cups into csv as all_world_cup_winners
+world_cup_matches.to_csv('Clean_Data/all_world_cup_winners.csv')
